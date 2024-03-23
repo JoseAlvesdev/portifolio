e9bf28cbdafb9d6158aac641231f54395f4db4e0
josehenriques10 <josehenriques10@outlook.com>
2024-03-22 21:28:00 -0300

    Nova atualização dinâmica implementada.

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,29 +1,5 @@
 "use strict";
 
-function updateProfileInfo(profileData) {
-    const photo = document.querySelector('.js-profile-photo');
-    photo.src = profileData.photo;
-    photo.alt = profileData.name;
-
-    const name = document.querySelector('.js-profile-name');
-    name.innerText = profileData.name;
-    name.style.fontWeight = '700';
-
-    const job = document.querySelector('.js-profile-job');
-    job.innerText = profileData.job;
-
-    const location = document.querySelector('.js-profile-location');
-    location.innerText = profileData.location;
-
-    const phone = document.querySelector('.js-profile-phone');
-    phone.href = `tel: ${profileData.phone}`;
-    phone.innerText = profileData.phone;
-
-    const email = document.querySelector('.js-profile-email');
-    email.href = `mailto:${profileData.email}`;
-    email.innerText = profileData.email;
-}
-
 async function fetchProfileData() {
     const url = 'https://raw.githubusercontent.com/josehenriques10/portifolio/main/data/profile.json';
 
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,41 @@
 "use strict";
 
 (function() {
-    (async function(){
+    function updateProfileInfo(profileData) {
+        const photo = document.querySelector('.js-profile-photo');
+        photo.src = profileData.photo;
+        photo.alt = profileData.name;
+    
+        const name = document.querySelector('.js-profile-name');
+        name.innerText = profileData.name;
+        name.style.fontWeight = '700';
+    
+        const job = document.querySelector('.js-profile-job');
+        job.innerText = profileData.job;
+    
+        const location = document.querySelector('.js-profile-location');
+        location.innerText = profileData.location;
+    
+        const phone = document.querySelector('.js-profile-phone');
+        phone.href = `tel: ${profileData.phone}`;
+        phone.innerText = profileData.phone;
+    
+        const email = document.querySelector('.js-profile-email');
+        email.href = `mailto:${profileData.email}`;
+        email.innerText = profileData.email;
+    }
+
+    function updateSoftSkills(profileData) {
+        const softskills = document.querySelector('.js-profile-softskills');
+        
+        softskills.innerHTML = profileData.skills.softSkills.map(
+            skill => `<li class="c-personal-list__item">${skill}</li>`
+        ).join('');
+    }
+
+    (async function() {
         const profileData = await fetchProfileData();
         updateProfileInfo(profileData);
-        console.log(profileData);
+        updateSoftSkills(profileData);
     })();
 })();
